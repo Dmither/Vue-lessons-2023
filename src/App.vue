38d3e9fdd6a56da181a34bd4df9b4da06d06dc08
Dmither <mr.d.graumann@gmail.com>
2023-10-31 17:15:32 +0200

    Create repo

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+import { reactive } from 'vue';
+
+const message = ref("Hello, World")
+
+console.log(message)
+console.log(message.value)
+message.value = "Changed"
+
+console.log(message.value)
+
+</script>
+
+<template>
+  <h1>Hello, Vue World!</h1>
+</template>
