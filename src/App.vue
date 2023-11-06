2fa585bf236cbc63c02245e11fa9ba57f7965e57
Dmither <mr.d.graumann@gmail.com>
2023-11-06 17:30:36 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive, computed } from 'vue';
 
-const obj = ref({
-  nasted: {count: 0},
-  arr: ["foo", "bar"],
-});
 
-function mutateDeeply() {
-  obj.value.nasted.count++
-}
 
 </script>
 
 <template>
-  <button @click="mutateDeeply">{{ obj.nasted.count }}</button>
 </template>
