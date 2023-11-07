00fb35215a2ee80b697c54a5c761605e1f27dfd8
Dmither <mr.d.graumann@gmail.com>
2023-11-07 16:49:20 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
-import { ref, reactive, computed } from 'vue';
+import { ref, reactive, computed, onMounted } from 'vue';
 
+const text1 = ref("");
+const text2 = ref("");
 
+onMounted(() => {
+  console.log(`Now the component has mounted`)
+})
 
 </script>
 
 <template>
+  <p>Input1: {{ text1 }}</p>
+  <input type="text" v-model="text1">
+  <p>Input2: {{ text2 }}</p>
+  <input type="text" v-model.lazy="text2">
 </template>
