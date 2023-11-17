21e9ccab5c1e9d384ceae08366f26ad17e4fd590
Dmither <mr.d.graumann@gmail.com>
2023-11-17 14:24:11 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
-<script setup>
-import { ref, computed, provide } from 'vue';
-import MyComponent from './components/MyComponent.vue';
-
-const val = ref(0);
-provide('key', val);
-console.log(val)
-
-</script>
-
 <template>
-  <MyComponent></MyComponent>
+  <div class="wrapper">
+    <Login v-if="show" @closeLogin="() => show = false" />
+    <button @click="() => show = true">Log in</button>
+  </div>
 </template>
 
+<script setup>
+import { ref, defineAsyncComponent } from 'vue';
+
+const Login = defineAsyncComponent(() => import("./components/Login.vue"));
+
+const show = ref(false);
+</script>
+
+<style>
+.wrapper {
+  padding: 15px;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="login-background" @click="cancel">
+    <form class="login-wrapper" @submit.prevent="submit">
+      <h4 class="login-title">Login to your account</h4>
+      <div class="login-field">
+        <label for="emain">Email</label>
+        <input type="email" name="email" id="email">
+      </div>
+      <div class="login-field login-pass">
+        <label for="password">Password</label>
+        <input :type="showpass ? 'text' : 'password'" name="password" id="password">
+        <input type="checkbox" class="login-showpass" v-model="showpass">
+      </div>
+      <button class="login-btn" type="submit">Log In</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const emit = defineEmits(["closeLogin"]);
+
+const showpass = ref(false);
+
+function cancel(event) {
+  if (event.target.classList.contains("login-background")) {
+    emit("closeLogin");
+  }
+}
+
+function submit() {
+  emit("closeLogin");
+}
+
+</script>
+
+<style scoped>
+.login-background {
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  right: 0;
+  background-color: #00000077;
+  padding: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.login-wrapper {
+  padding: 15px;
+  background-color: #333;
+  max-width: 400px;
+  height: min-content;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.login-title {}
+
+.login-field {
+  display: flex;
+  flex-direction: column;
+}
+
+.login-pass {
+  position: relative;
+}
+
+.login-showpass {
+  position: absolute;
+  bottom: 3px;
+  right: 3px;
+}
+
+.login-btn {}
+</style>
